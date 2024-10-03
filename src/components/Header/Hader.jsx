6d89894ce6974d156736a6f225b76a96a5048c7b
Tanvir <tanvir.info07@gmail.com>
2024-10-04 01:14:53 +0600

    header content image path fixed

--- src/components/Header/Hader.jsx
+++ src/components/Header/Hader.jsx
@@ -2,7 +2,7 @@ import { Button } from "../ui/button";
 
 const Hader = () => {
   return (
-    <div className="bg-[url('./public/header_img.png')] h-[34vw]  bg-contain my-auto mx-5 sm:mx-7 bg-no-repeat relative md:mt-4 mt-2 ">
+    <div className="bg-[url('/header_img.png')] h-[34vw]  bg-contain my-auto mx-5 sm:mx-7 bg-no-repeat relative md:mt-4 mt-2 ">
       <div className="absolute flex flex-col items-start gap-[1.5vw] max-w-[50%] md:bottom-[15%] bottom-[10%] left-[6vw] text-white ">
         <h1 className="font-medium sm:text-[max(3.5vw,24px)] text-[18px] ">
           Order Your Favorite Food Here
